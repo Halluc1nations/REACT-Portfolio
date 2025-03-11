eab4c49918b408a7f0c0763db6839d6c47f1ec4e
Halluc1nations <Blaine.Bishop@comcast.net>
2025-03-11 16:27:24 -0400

    CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Header({ currentPage, setCurrentPage }) {
   return (
-    <header className="bg-gray-900 text-white p-4 flex justify-between items-center">
+    <header className="bg-gray-900 p-4 flex justify-between items-center">
       <h1 className="text-2xl font-bold">Blaine Bishop</h1>
       <nav>
         <ul className="flex space-x-4">
           {["About", "Portfolio", "Contact", "Resume"].map((title) => (
             <li
               key={title}
-              className={`cursor-pointer ${currentPage === title ? "text-yellow-400" : "text-white"
+              className={`cursor-pointer ${currentPage === title ? "text-yellow" : "text-black"
                 }`}
               onClick={() => setCurrentPage(title)}
             >
--- src/components/Navtab.jsx
+++ src/components/Navtab.jsx
@@ -1,41 +1,41 @@
-import React from "react";
+import "react";
 import { Link, useLocation } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 function Navigation({ currentPage, setCurrentPage }) {
-  const location = useLocation(); // Get current route
+    const location = useLocation(); // Get current route
 
-  return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        {/* Navbar Brand */}
-        <Navbar.Brand as={Link} to="/" onClick={() => setCurrentPage("About")}>
-          Developer Name
-        </Navbar.Brand>
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+            <Container>
+                {/* Navbar Brand */}
+                <Navbar.Brand as={Link} to="/" onClick={() => setCurrentPage("About")}>
+                    Developer Name
+                </Navbar.Brand>
 
-        {/* Hamburger Menu */}
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto">
-            {/* Navigation Links */}
-            {["About", "Portfolio", "Contact", "Resume"].map((title) => (
-              <Nav.Link
-                key={title}
-                as={Link}
-                to={`/${title.toLowerCase()}`}
-                onClick={() => setCurrentPage(title)}
-                className={`text-light mx-3 ${
-                  location.pathname === `/${title.toLowerCase()}` ? "fw-bold text-warning" : ""
-                }`}
-              >
-                {title}
-              </Nav.Link>
-            ))}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+                {/* Hamburger Menu */}
+                <Navbar.Toggle aria-controls="navbar-nav" />
+                
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="ms-auto">
+                        {/* Navigation Links */}
+                        {["About", "Portfolio", "Contact", "Resume"].map((title) => (
+                            <Nav.Link
+                                key={title}
+                                as={Link}
+                                to={`/${title.toLowerCase()}`}
+                                onClick={() => setCurrentPage(title)}
+                                className={`text-light mx-3 ${location.pathname === `/${title.toLowerCase()}` ? "fw-bold text-warning" : ""
+                                    }`}
+                            >
+                                {title}
+                            </Nav.Link>
+                        ))}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default Navigation;
